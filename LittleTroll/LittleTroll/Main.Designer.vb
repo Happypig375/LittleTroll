d0e1c8d027db8f629e67a11d29bda1dbe8f4465f
Happypig375 <hadrianwttang@outlook.com>
2016-04-16 22:03:17 +0800

    First editing!!

---
+++ LittleTroll/LittleTroll/Main.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.Tabs = New System.Windows.Forms.TabControl()
+        Me.SoundEmitter = New System.Windows.Forms.TabPage()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Settings = New System.Windows.Forms.TabPage()
+        Me.Tabs.SuspendLayout()
+        Me.SoundEmitter.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Tabs
+        '
+        Me.Tabs.Controls.Add(Me.SoundEmitter)
+        Me.Tabs.Controls.Add(Me.Settings)
+        Me.Tabs.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Tabs.Location = New System.Drawing.Point(0, 0)
+        Me.Tabs.Name = "Tabs"
+        Me.Tabs.SelectedIndex = 0
+        Me.Tabs.Size = New System.Drawing.Size(284, 261)
+        Me.Tabs.TabIndex = 0
+        '
+        'SoundEmitter
+        '
+        Me.SoundEmitter.Controls.Add(Me.CheckBox1)
+        Me.SoundEmitter.Location = New System.Drawing.Point(4, 22)
+        Me.SoundEmitter.Name = "SoundEmitter"
+        Me.SoundEmitter.Padding = New System.Windows.Forms.Padding(3)
+        Me.SoundEmitter.Size = New System.Drawing.Size(276, 235)
+        Me.SoundEmitter.TabIndex = 0
+        Me.SoundEmitter.Text = "SoundEmitter"
+        Me.SoundEmitter.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(27, 86)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Settings
+        '
+        Me.Settings.Location = New System.Drawing.Point(4, 22)
+        Me.Settings.Name = "Settings"
+        Me.Settings.Padding = New System.Windows.Forms.Padding(3)
+        Me.Settings.Size = New System.Drawing.Size(276, 235)
+        Me.Settings.TabIndex = 1
+        Me.Settings.Text = "Settings"
+        Me.Settings.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Tabs)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Main"
+        Me.ShowInTaskbar = False
+        Me.Text = "LittleTroll"
+        Me.Tabs.ResumeLayout(False)
+        Me.SoundEmitter.ResumeLayout(False)
+        Me.SoundEmitter.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Tabs As System.Windows.Forms.TabControl
+    Friend WithEvents SoundEmitter As System.Windows.Forms.TabPage
+    Friend WithEvents Settings As System.Windows.Forms.TabPage
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
