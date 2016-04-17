70696b8aa0844d9f0dbfe46206b6108ea4bb30d0
Happypig375 <hadrianwttang@outlook.com>
2016-04-17 20:30:52 +0800

    Changing the first tab around

--- LittleTroll/LittleTroll/Main.Designer.vb
+++ LittleTroll/LittleTroll/Main.Designer.vb
@@ -24,36 +24,56 @@ Partial Class Main
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
         Me.Tabs = New System.Windows.Forms.TabControl()
-        Me.SoundEmitter = New System.Windows.Forms.TabPage()
+        Me.MediaEmitter = New System.Windows.Forms.TabPage()
+        Me.PickTime = New System.Windows.Forms.DateTimePicker()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.Settings = New System.Windows.Forms.TabPage()
-        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.AddSchedule = New System.Windows.Forms.Button()
+        Me.RemoveSchedule = New System.Windows.Forms.Button()
+        Me.UpdateSchedule = New System.Windows.Forms.Button()
+        Me.ButtonLayout = New System.Windows.Forms.TableLayoutPanel()
+        Me.List = New System.Windows.Forms.ListView()
+        Me.Time = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.Tabs.SuspendLayout()
-        Me.SoundEmitter.SuspendLayout()
+        Me.MediaEmitter.SuspendLayout()
+        Me.ButtonLayout.SuspendLayout()
         Me.SuspendLayout()
         '
         'Tabs
         '
-        Me.Tabs.Controls.Add(Me.SoundEmitter)
+        Me.Tabs.Controls.Add(Me.MediaEmitter)
         Me.Tabs.Controls.Add(Me.Settings)
         Me.Tabs.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Tabs.Location = New System.Drawing.Point(0, 0)
         Me.Tabs.Name = "Tabs"
         Me.Tabs.SelectedIndex = 0
-        Me.Tabs.Size = New System.Drawing.Size(284, 261)
+        Me.Tabs.Size = New System.Drawing.Size(331, 261)
         Me.Tabs.TabIndex = 0
         '
-        'SoundEmitter
+        'MediaEmitter
         '
-        Me.SoundEmitter.Controls.Add(Me.DateTimePicker1)
-        Me.SoundEmitter.Controls.Add(Me.CheckBox1)
-        Me.SoundEmitter.Location = New System.Drawing.Point(4, 22)
-        Me.SoundEmitter.Name = "SoundEmitter"
-        Me.SoundEmitter.Padding = New System.Windows.Forms.Padding(3)
-        Me.SoundEmitter.Size = New System.Drawing.Size(276, 235)
-        Me.SoundEmitter.TabIndex = 0
-        Me.SoundEmitter.Text = "SoundEmitter"
-        Me.SoundEmitter.UseVisualStyleBackColor = True
+        Me.MediaEmitter.Controls.Add(Me.List)
+        Me.MediaEmitter.Controls.Add(Me.ButtonLayout)
+        Me.MediaEmitter.Controls.Add(Me.PickTime)
+        Me.MediaEmitter.Controls.Add(Me.CheckBox1)
+        Me.MediaEmitter.Location = New System.Drawing.Point(4, 22)
+        Me.MediaEmitter.Name = "MediaEmitter"
+        Me.MediaEmitter.Padding = New System.Windows.Forms.Padding(3)
+        Me.MediaEmitter.Size = New System.Drawing.Size(323, 235)
+        Me.MediaEmitter.TabIndex = 0
+        Me.MediaEmitter.Text = "MediaEmitter"
+        Me.MediaEmitter.UseVisualStyleBackColor = True
+        '
+        'PickTime
+        '
+        Me.PickTime.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PickTime.CustomFormat = "dd/MM/yyyy dddd HH:mm:ss"
+        Me.PickTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.PickTime.Location = New System.Drawing.Point(8, 30)
+        Me.PickTime.Name = "PickTime"
+        Me.PickTime.Size = New System.Drawing.Size(307, 20)
+        Me.PickTime.TabIndex = 1
         '
         'CheckBox1
         '
@@ -75,33 +95,95 @@ Partial Class Main
         Me.Settings.Text = "Settings"
         Me.Settings.UseVisualStyleBackColor = True
         '
-        'DateTimePicker1
+        'AddSchedule
         '
-        Me.DateTimePicker1.Location = New System.Drawing.Point(8, 30)
-        Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(183, 20)
-        Me.DateTimePicker1.TabIndex = 1
+        Me.AddSchedule.Location = New System.Drawing.Point(3, 3)
+        Me.AddSchedule.Name = "AddSchedule"
+        Me.AddSchedule.Size = New System.Drawing.Size(87, 23)
+        Me.AddSchedule.TabIndex = 2
+        Me.AddSchedule.Text = "Add Schedule"
+        Me.AddSchedule.UseVisualStyleBackColor = True
+        '
+        'RemoveSchedule
+        '
+        Me.RemoveSchedule.Location = New System.Drawing.Point(96, 3)
+        Me.RemoveSchedule.Name = "RemoveSchedule"
+        Me.RemoveSchedule.Size = New System.Drawing.Size(104, 23)
+        Me.RemoveSchedule.TabIndex = 3
+        Me.RemoveSchedule.Text = "Remove Schedule"
+        Me.RemoveSchedule.UseVisualStyleBackColor = True
+        '
+        'UpdateSchedule
+        '
+        Me.UpdateSchedule.Location = New System.Drawing.Point(206, 3)
+        Me.UpdateSchedule.Name = "UpdateSchedule"
+        Me.UpdateSchedule.Size = New System.Drawing.Size(100, 23)
+        Me.UpdateSchedule.TabIndex = 4
+        Me.UpdateSchedule.Text = "Update Schedule"
+        Me.UpdateSchedule.UseVisualStyleBackColor = True
+        '
+        'ButtonLayout
+        '
+        Me.ButtonLayout.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonLayout.ColumnCount = 3
+        Me.ButtonLayout.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.89372!))
+        Me.ButtonLayout.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.10628!))
+        Me.ButtonLayout.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 105.0!))
+        Me.ButtonLayout.Controls.Add(Me.AddSchedule, 0, 0)
+        Me.ButtonLayout.Controls.Add(Me.UpdateSchedule, 2, 0)
+        Me.ButtonLayout.Controls.Add(Me.RemoveSchedule, 1, 0)
+        Me.ButtonLayout.Location = New System.Drawing.Point(8, 56)
+        Me.ButtonLayout.Name = "ButtonLayout"
+        Me.ButtonLayout.RowCount = 1
+        Me.ButtonLayout.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.ButtonLayout.Size = New System.Drawing.Size(309, 31)
+        Me.ButtonLayout.TabIndex = 5
+        '
+        'List
+        '
+        Me.List.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.List.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.Time})
+        Me.List.Location = New System.Drawing.Point(6, 93)
+        Me.List.Name = "List"
+        Me.List.Size = New System.Drawing.Size(308, 136)
+        Me.List.TabIndex = 6
+        Me.List.UseCompatibleStateImageBehavior = False
+        Me.List.View = System.Windows.Forms.View.Details
+        '
+        'Time
+        '
+        Me.Time.Text = "Time"
+        Me.Time.Width = 57
         '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(331, 261)
         Me.Controls.Add(Me.Tabs)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Main"
         Me.ShowInTaskbar = False
         Me.Text = "LittleTroll"
         Me.Tabs.ResumeLayout(False)
-        Me.SoundEmitter.ResumeLayout(False)
-        Me.SoundEmitter.PerformLayout()
+        Me.MediaEmitter.ResumeLayout(False)
+        Me.MediaEmitter.PerformLayout()
+        Me.ButtonLayout.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Tabs As System.Windows.Forms.TabControl
-    Friend WithEvents SoundEmitter As System.Windows.Forms.TabPage
+    Friend WithEvents MediaEmitter As System.Windows.Forms.TabPage
     Friend WithEvents Settings As System.Windows.Forms.TabPage
     Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
-    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents PickTime As System.Windows.Forms.DateTimePicker
+    Friend WithEvents ButtonLayout As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents UpdateSchedule As System.Windows.Forms.Button
+    Friend WithEvents RemoveSchedule As System.Windows.Forms.Button
+    Friend WithEvents AddSchedule As System.Windows.Forms.Button
+    Friend WithEvents List As System.Windows.Forms.ListView
+    Friend WithEvents Time As System.Windows.Forms.ColumnHeader
 
 End Class
--- LittleTroll/LittleTroll/Main.vb
+++ LittleTroll/LittleTroll/Main.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Main
 
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles ButtonLayout.Paint
+
+    End Sub
 End Class
